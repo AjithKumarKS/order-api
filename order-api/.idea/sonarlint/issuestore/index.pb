6911c710a409f85b9283105f437debd9f0baac52
pavan8123 <bantakuntapavan@gmail.com>
2019-09-09 20:08:51 +0530

    Created OrderApiApplication

---
+++ order-api/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+k
+;src/main/java/com/virtusa/order_api/OderApiApplication.java,f\c\fc65a080939b1802bc0aa0953c76cdb17868621f
---
+++ order-api/src/main/java/com/virtusa/order_api/OderApiApplication.java
@@ -0,0 +1,12 @@
+package com.virtusa.order_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OderApiApplication.class,args);
+    }
+}
